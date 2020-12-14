1c62380f60e5484e6fd9ffb720aacdf6ffc389c9
Legattto <slarku@yandex.ru>
2020-12-14 23:43:21 +0400

    da
    s

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 let numberDice = document.querySelector("#dice");
 let btnRoll = document.querySelector("#roll");
+let diceCount = document.querySelector("#dicecount");
 let rollCount = document.querySelector("#rollcount");
+let rusultInsert = document.querySelector("#result");
 
 btnRoll.addEventListener("click", function (elem) {
+  rusultInsert.innerHTML = "";
   elem.preventDefault();
   let valueOption = numberDice.value;
   let valueRollcount = rollCount.value;
+  let diceCountValue = diceCount.value;
   let result;
   let min = 1;
+  let sum = 0;
   let max = Math.floor(valueOption);
   for (let i = 0; i < valueRollcount; i++) {
-    result = Math.floor(Math.random() * (max - min + 1)) + min;
+    for (let i = 0; i < diceCountValue; i++) {
+      result = Math.floor(Math.random() * (max - min + 1)) + min;
+      sum = sum + result;
+    }
+    rusultInsert.insertAdjacentText("beforeend", sum + ',' + "   ");
+    sum = 0;
   };
-  console.log(result);
+  rusultInsert.style.display = "flex";
 });
